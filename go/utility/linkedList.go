f9ff054e34b75d6f7ca0ebc90c185025409cb04c
Abdul Rahim Ovingal <abdulearningit@gmail.com>
2024-07-05 10:18:36 +0530

    utility pkg: linked list related fns added

---
+++ go/utility/linkedList.go
@@ -0,0 +1,51 @@
+package utility
+
+import "fmt"
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func CreateList(nums []int) *ListNode {
+	length := len(nums)
+	if length == 0 {
+		return &ListNode{}
+	}
+	if length == 1 {
+		return &ListNode{
+			Val: nums[0],
+		}
+	}
+
+	head := &ListNode{
+		Val: nums[0],
+		Next: &ListNode{
+			Val: nums[1],
+		},
+	}
+	current := head
+	for i := 2; i < length; i++ {
+		current.Next.Next = &ListNode{
+			Val: nums[i],
+		}
+		current = current.Next
+	}
+	return head
+}
+func (head *ListNode)DisplayList() {
+	fmt.Print("List= ")
+	if head == nil {
+		fmt.Println("nil")
+		return
+	} else {
+		fmt.Print(head.Val)
+	}
+	current := head.Next
+	for current != nil {
+		fmt.Print(",", current.Val)
+		current = current.Next
+	}
+	fmt.Println()
+}
