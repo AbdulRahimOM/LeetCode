cc030261b86eb2f3899bc4031eb76bb99ea9f622
Abdul Rahim Ovingal <abdulearningit@gmail.com>
2024-07-22 00:30:38 +0530

    compare matrix utility

--- go/utility/matrix.go
+++ go/utility/matrix.go
@@ -7,3 +7,22 @@ func PrintMatrix(m [][]int) {
 		fmt.Println(r)
 	}
 }
+func CompareMatrixes(a, b [][]int) bool {
+	// Check if outer slice lengths are the same
+	if len(a) != len(b) {
+		return false
+	}
+
+	// Check if inner slice lengths and elements are the same
+	for i := range a {
+		if len(a[i]) != len(b[i]) {
+			return false
+		}
+		for j := range a[i] {
+			if a[i][j] != b[i][j] {
+				return false
+			}
+		}
+	}
+	return true
+}
